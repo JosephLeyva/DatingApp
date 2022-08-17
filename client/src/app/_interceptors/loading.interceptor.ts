90ab36e701882a5ac25216095236f05d4a6ea120
Jose Leyva <josealc1998@gmail.com>
2022-08-17 14:38:46 -0700

    Section 19

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -17,7 +17,6 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busiService.busy();
     return next.handle(request).pipe(
-      delay(1000),
       finalize(() => {
         this.busiService.idle();
       })
